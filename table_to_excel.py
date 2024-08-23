65c2c0dc005d839932789126d7ca1775ea96d15f
Tushar <tusharkumar128@gmail.com>
2024-08-23 15:20:57 +0530

    table_scrapping

--- table_to_excel.py
+++ table_to_excel.py
@@ -3,18 +3,18 @@ from pdf2image import convert_from_path
 import pandas as pd
 
 # Path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Update with your Tesseract path
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Update with your Tesseract path
 
 # Path to the PDF file
-pdf_path = "C:\Users\ZANG\OneDrive\Desktop\Puneet\Solar_Radian_Energy_Over_India.pdf"  # Update with your PDF path
+pdf_path = r"C:\Users\ZANG\OneDrive\Desktop\Puneet\Solar_Radian_Energy_Over_India.pdf"  # Update with your PDF path
 
 # Convert the specific page of the PDF to an image (e.g., page 100)
-page_number = 292  # Update with the correct page number
+page_number = 291  # Update with the correct page number
 pages = convert_from_path(pdf_path, first_page=page_number, last_page=page_number)
 
 # Save the page as an image
-image_path = "page_image.png"
-pages[1].save(image_path, 'PNG')
+image_path = r"page_image.png"
+pages[0].save(image_path, 'PNG')
 
 # Perform OCR to extract text from the image
 text = pytesseract.image_to_string(image_path)
@@ -39,7 +39,7 @@ df.columns = df.iloc[0]
 df = df[1:]
 
 # Save the DataFrame to an Excel file
-excel_path ='extracted_table.xlsx'  # Update with desired output path
+excel_path =r'extracted_table.xlsx'  # Update with desired output path
 df.to_excel(excel_path, index=False)
 
 print(f"Data has been extracted and saved to {excel_path}")
